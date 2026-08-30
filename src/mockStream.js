5bfcc3044a5c2fea76bb61244f4337db881ee7c9
Test Rig <halvin@red-dolphin.co.uk>
2026-08-30 14:01:20 +0000

    iter-slice3-001-iter-003: builder patch for src/mockStream.js

--- src/mockStream.js
+++ src/mockStream.js
@@ -4,6 +4,36 @@
 // break_started; every task_started mints a new task name "task-N".
 
 export function createMockStream(agentIds) {
-  // TODO: return { next() } producing the deterministic sequence
-  return { next: () => null };
+  let agentIndex = 0;
+  let taskNumber = 1;
+  const eventTypes = ['task_started', 'task_completed', 'message_sent', 'break_started'];
+  let eventTypeIndex = 0;
+
+  return {
+    next() {
+      const agent = agentIds[agentIndex];
+      let event;
+
+      if (eventTypes[eventTypeIndex] === 'task_started') {
+        event = {
+          type: 'task_started',
+          agent: agent,
+          task: `task-${taskNumber++}`
+        };
+      } else {
+        event = {
+          type: eventTypes[eventTypeIndex],
+          agent: agent
+        };
+      }
+
+      // Move to next agent (round-robin)
+      agentIndex = (agentIndex + 1) % agentIds.length;
+
+      // Move to next event type
+      eventTypeIndex = (eventTypeIndex + 1) % eventTypes.length;
+
+      return event;
+    }
+  };
 }
